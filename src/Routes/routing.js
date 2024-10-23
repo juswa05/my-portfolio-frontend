5e957a11884bb095c49934cc3c891776e96b4dab
Jushua Bornales <joshbornales0@gmail.com>
2024-10-23 21:03:03 +0800

    Fixed Routing

--- src/Routes/routing.js
+++ src/Routes/routing.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from '../components/Home';
+import About from '../components/About';
+import Contact from '../components/Contact';
+import LandingPage from '../components/LandingPage'; // Import the landing page component
 
-const Routes = () => {
-  return (
+const Routing = () => (
     <Router>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+        <Routes>
+            <Route path="/" element={<LandingPage />} /> {/* Set LandingPage as the default route */}
+            <Route path="/home" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
     </Router>
-  );
-};
+);
 
-export default Routes;
+export default Routing;
